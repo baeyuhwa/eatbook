b0d6056a585c7e3a6396c16e354f609e78561858
baeyuhwa <qodbghk23@gmail.com>
2024-05-25 03:58:16 +0900

    9,10 대충 수정

---
+++ 9_commincation.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    // 저장된 게시물 가져오기
+    var storedPost = localStorage.getItem('newPost');
+    if (storedPost) {
+        var post = JSON.parse(storedPost);
+        displayPost(post);
+    }
+};
+
+function displayPost(post) {
+    var tableBody = document.querySelector('.content table tbody');
+    var row = document.createElement('tr');
+    row.innerHTML = "<td>" + post.title + "</td><td>" + post.content + "</td><td>" + new Date().toLocaleDateString() + "</td>";
+    tableBody.appendChild(row);
+}
