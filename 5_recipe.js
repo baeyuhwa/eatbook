6d13f78fae66a39851ff2dbdb73bc8829c573f6b
euna22 <hknu810075@gmail.com>
2024-05-30 21:01:14 +0900

    모든 페이지에서 검색 가능하도록 수정

--- 5_recipe.js
+++ 5_recipe.js
@@ -1,12 +1,12 @@
 window.onload = function() {
     document.getElementById('search-bar').addEventListener('keyup', function(event) {
         if(event.key == "Enter") {
-            perfomSearch();
+            performSearch();
         }
     });
 }
 
-function perfomSearch() {
+function performSearch() {
     const searchVlalue = document.getElementById('search-bar').value;
     localStorage.setItem('searchQuery', searchVlalue);
     window.open('5_recipe_result.html', '_blank');
